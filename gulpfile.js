d798d87b394db13e6408e949fcd654b5409efef4
nat6 <antnatalia55@gmail.com>
2020-02-19 17:13:08 +0300

    Add 1-4 sass block

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@
 var gulp = require('gulp'),
 		pug = require('gulp-pug'),
 		sass = require('gulp-sass');
-
 gulp.task('pug', function() {
 	return gulp.src('src/*.pug')
 		.pipe(pug({
 			pretty:true
 		}))
 		.pipe(gulp.dest('build'));
-	})
+	});
+gulp.task('sass', function(){
+	return gulp.src('src/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('build/styles'))
+});
+gulp.task('watch', function() {
+	gulp.watch('src/*.scss', gulp.parallel('sass'));
+	gulp.watch('src/*.pug', gulp.parallel('pug'))
+});
+gulp.task( 'default', gulp.parallel('sass', 'pug', 'watch'));
